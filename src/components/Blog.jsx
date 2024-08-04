67f556dd893ce17bf77bca42feebf655088b7b10
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 13:56:47 +0200

    solve ex5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+
 export function Blog({ blog }) {
+  const [expanded, setExpanded] = useState(false)
+  const toggleExpanded = () => setExpanded((expanded) => !expanded)
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div
+      style={{
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+      }}
+    >
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button type="button" onClick={toggleExpanded}>
+          {expanded ? 'hide' : 'view'}
+        </button>
+      </div>
+      {expanded ? (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button type="button">like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      ) : null}
     </div>
-  );
+  )
 }
