3e38f12c0b8f9297e04054bfdb095479ca9bb223
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 20:01:35 +0200

    solve ex5.14

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { expect, test } from 'vitest'
 import { Blog } from './Blog.jsx'
 
@@ -14,4 +15,22 @@ test('shows title and author, but hides url or likes by default', async () => {
 
   expect(screen.getByText(/React patterns/i)).toBeVisible()
   expect(screen.getByText(/Michael Chan/i)).toBeVisible()
+})
+
+test('url and likes are rendered when expanded', async () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+
+  await user.click(screen.getByRole('button', { name: /view/i }))
+
+  expect(screen.getByText(/reactpatterns.com/i)).toBeVisible()
+  expect(screen.getByText(/likes 7/i)).toBeVisible()
 })
