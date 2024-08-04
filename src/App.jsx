3a349454650715e0383503f833b2bab1489bfed2
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 11:26:44 +0200

    solve ex5.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Blog } from "./components/Blog.jsx";
-import { getAllBlogs } from "./services/blogs.js";
+import { createBlog, getAllBlogs, setToken } from "./services/blogs.js";
 import { login } from "./services/login.js";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       const rawUser = localStorage.getItem("user");
       if (rawUser) {
         const user = JSON.parse(rawUser);
+        setToken(user.token);
         return user;
       }
 
@@ -25,26 +26,57 @@ function App() {
 
   return user ? (
     <>
-      <h2>blogs</h2>
+      <h1>blogs</h1>
       <p>
         {user.name ?? user.username} logged in{" "}
         <button
           type="button"
           onClick={() => {
             setUser(null);
+            setToken(null);
             localStorage.removeItem("user");
           }}
         >
           logout
         </button>
       </p>
+      <h2>Create new</h2>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          const form = event.target;
+          const formData = new FormData(form);
+
+          try {
+            const title = formData.get("title")?.toString();
+            const author = formData.get("author")?.toString();
+            const url = formData.get("url")?.toString();
+            const blog = await createBlog({ title, author, url });
+            setBlogs((blogs) => blogs.concat(blog));
+
+            form.reset();
+          } catch {}
+        }}
+      >
+        <div>
+          title <input type="text" name="title" />
+        </div>
+        <div>
+          author <input type="text" name="author" />
+        </div>
+        <div>
+          url <input type="text" name="url" />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
     </>
   ) : (
     <>
-      <h2>Log in to application</h2>
+      <h1>Log in to application</h1>
       <form
         onSubmit={async (event) => {
           event.preventDefault();
@@ -56,6 +88,7 @@ function App() {
           const password = formData.get("password")?.toString();
           const user = await login({ username, password });
           setUser(user);
+          setToken(user.token);
           localStorage.setItem("user", JSON.stringify(user));
 
           form.reset();
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,7 +2,21 @@ import axios from "axios";
 
 const BASE_URL = "/api/blogs";
 
-export const getAllBlogs = async () => {
+let token = null;
+export const setToken = (newToken) => (token = newToken);
+
+export async function getAllBlogs() {
   const response = await axios.get(BASE_URL);
   return response.data;
-};
+}
+
+export async function createBlog(blogObject) {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.post(BASE_URL, blogObject, config);
+
+  return response.data;
+}
