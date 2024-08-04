367850417636e200979bea73e9b0094e413aa4b0
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 13:48:35 +0200

    hide form

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,92 @@
-import { useEffect, useState } from "react";
-import { Blog } from "./components/Blog.jsx";
-import { createBlog, getAllBlogs, setToken } from "./services/blogs.js";
-import { login } from "./services/login.js";
-import { AlertBox } from "./components/AlertBox.jsx";
+import { useEffect, useState } from 'react'
+import { AlertBox } from './components/AlertBox.jsx'
+import { Blog } from './components/Blog.jsx'
+import { Togglable } from './components/Togglable.jsx'
+import { createBlog, getAllBlogs, setToken } from './services/blogs.js'
+import { login } from './services/login.js'
 
 function App() {
-  const [alert, setAlert] = useState(null);
+  const [alert, setAlert] = useState(null)
   const notify = (alert, timeoutMs = 5000) => {
-    setAlert(alert);
-    window.setTimeout(() => setAlert(null), timeoutMs);
-  };
+    setAlert(alert)
+    window.setTimeout(() => setAlert(null), timeoutMs)
+  }
 
   const [user, setUser] = useState(() => {
     try {
-      const rawUser = localStorage.getItem("user");
+      const rawUser = localStorage.getItem('user')
       if (rawUser) {
-        const user = JSON.parse(rawUser);
-        setToken(user.token);
-        return user;
+        const user = JSON.parse(rawUser)
+        setToken(user.token)
+        return user
       }
 
-      return null;
+      return null
     } catch (error) {
-      return null;
+      return null
     }
-  });
-  const [blogs, setBlogs] = useState([]);
+  })
+  const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    getAllBlogs().then(setBlogs);
-  }, []);
+    getAllBlogs().then(setBlogs)
+  }, [])
 
   return user ? (
     <>
       <h1>blogs</h1>
       <AlertBox alert={alert} />
       <p>
-        {user.name ?? user.username} logged in{" "}
+        {user.name ?? user.username} logged in{' '}
         <button
           type="button"
           onClick={() => {
-            setUser(null);
-            setToken(null);
-            localStorage.removeItem("user");
+            setUser(null)
+            setToken(null)
+            localStorage.removeItem('user')
           }}
         >
           logout
         </button>
       </p>
-      <h2>Create new</h2>
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
+      <Togglable openButtonLabel="new note">
+        <h2>Create new</h2>
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault()
 
-          const form = event.target;
-          const formData = new FormData(form);
+            const form = event.target
+            const formData = new FormData(form)
 
-          try {
-            const title = formData.get("title")?.toString();
-            const author = formData.get("author")?.toString();
-            const url = formData.get("url")?.toString();
-            const blog = await createBlog({ title, author, url });
-            setBlogs((blogs) => blogs.concat(blog));
-            notify({
-              message: `A new blog "${blog.title}" by "${blog.author}" added`,
-              status: "success",
-            });
+            try {
+              const title = formData.get('title')?.toString()
+              const author = formData.get('author')?.toString()
+              const url = formData.get('url')?.toString()
+              const blog = await createBlog({ title, author, url })
+              setBlogs((blogs) => blogs.concat(blog))
+              notify({
+                message: `A new blog "${blog.title}" by "${blog.author}" added`,
+                status: 'success',
+              })
 
-            form.reset();
-          } catch (error) {
-            notify({ message: error.response.data.error, status: "error" });
-          }
-        }}
-      >
-        <div>
-          title <input type="text" name="title" />
-        </div>
-        <div>
-          author <input type="text" name="author" />
-        </div>
-        <div>
-          url <input type="url" name="url" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+              form.reset()
+            } catch (error) {
+              notify({ message: error.response.data.error, status: 'error' })
+            }
+          }}
+        >
+          <div>
+            title <input type="text" name="title" />
+          </div>
+          <div>
+            author <input type="text" name="author" />
+          </div>
+          <div>
+            url <input type="url" name="url" />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </Togglable>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
@@ -94,22 +97,22 @@ function App() {
       <AlertBox alert={alert} />
       <form
         onSubmit={async (event) => {
-          event.preventDefault();
+          event.preventDefault()
 
-          const form = event.target;
-          const formData = new FormData(form);
+          const form = event.target
+          const formData = new FormData(form)
 
           try {
-            const username = formData.get("username")?.toString();
-            const password = formData.get("password")?.toString();
-            const user = await login({ username, password });
-            setUser(user);
-            setToken(user.token);
-            localStorage.setItem("user", JSON.stringify(user));
-            form.reset();
-            notify({ message: "Successfully logged in", status: "success" });
+            const username = formData.get('username')?.toString()
+            const password = formData.get('password')?.toString()
+            const user = await login({ username, password })
+            setUser(user)
+            setToken(user.token)
+            localStorage.setItem('user', JSON.stringify(user))
+            form.reset()
+            notify({ message: 'Successfully logged in', status: 'success' })
           } catch (error) {
-            notify({ message: error.response.data.error, status: "error" });
+            notify({ message: error.response.data.error, status: 'error' })
           }
         }}
       >
@@ -122,7 +125,7 @@ function App() {
         <button type="submit">login</button>
       </form>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Togglable.jsx
@@ -0,0 +1,40 @@
+import { forwardRef, useImperativeHandle, useReducer } from 'react'
+
+export const Togglable = forwardRef(
+  (
+    {
+      defaultVisible = false,
+      openButtonLabel = 'open',
+      closeButtonLabel = 'close',
+      children,
+    },
+    ref,
+  ) => {
+    const [visible, toggleVisibile] = useReducer(
+      (visible) => !visible,
+      defaultVisible,
+    )
+
+    const hideWhenVisible = { display: visible ? 'none' : undefined }
+    const showWhenVisible = { display: visible ? undefined : 'none' }
+
+    useImperativeHandle(ref, () => {
+      return {
+        toggleVisibile,
+      }
+    })
+
+    return (
+      <div>
+        <button type="button" onClick={toggleVisibile} style={hideWhenVisible}>
+          {openButtonLabel}
+        </button>
+        <div style={showWhenVisible}>{children}</div>
+        <button type="button" onClick={toggleVisibile} style={showWhenVisible}>
+          {closeButtonLabel}
+        </button>
+      </div>
+    )
+  },
+)
+Togglable.displayName = 'Togglable'
