4f8994d09d536ca7eb2b5340d764f715b8a07207
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 20:04:57 +0200

    solve ex5.15

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 import { Blog } from './Blog.jsx'
 
 test('shows title and author, but hides url or likes by default', async () => {
@@ -33,4 +33,24 @@ test('url and likes are rendered when expanded', async () => {
 
   expect(screen.getByText(/reactpatterns.com/i)).toBeVisible()
   expect(screen.getByText(/likes 7/i)).toBeVisible()
+})
+
+test('if the like button is clicked twice, the event handler is called twice', async () => {
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  }
+  const mockOnUpdate = vi.fn()
+
+  render(<Blog blog={blog} onUpdate={mockOnUpdate} />)
+
+  const user = userEvent.setup()
+
+  await user.click(screen.getByRole('button', { name: /view/i }))
+  await user.click(screen.getByRole('button', { name: /like/i }))
+  await user.click(screen.getByRole('button', { name: /like/i }))
+
+  expect(mockOnUpdate).toHaveBeenCalledTimes(2)
 })
